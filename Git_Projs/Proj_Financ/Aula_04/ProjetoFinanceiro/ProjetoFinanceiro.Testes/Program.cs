5e958f659692506b3180162c9835a1fc4eed77a6
andreleaos <andreleaos@gmail.com>
2022-08-17 23:37:41 -0300

    Aulas 02 a 05

---
+++ Git_Projs/Proj_Financ/Aula_04/ProjetoFinanceiro/ProjetoFinanceiro.Testes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProjetoFinanceiro.Testes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidarCamadaDominio();
+            ValidarCamadaEstrutura_Context();
+        }
+
+        private static void ValidarCamadaEstrutura_Context()
+        {
+            FakeContextTeste teste = new FakeContextTeste();
+            teste.TestarListagem();
+            teste.TestarInclusao();
+        }
+
+        private static void ValidarCamadaDominio()
+        {
+            DominioTeste teste = new DominioTeste();
+            teste.TestarEntidade();
+            teste.TestarDto();
+            teste.TestarConversaoEntidadeParaDto();
+            teste.TestarConversaoDtoParaEntidade();
+        }
+    }
+}
---
+++ Git_Projs/Proj_Financ/Aula_05/ProjetoFinanceiro/ProjetoFinanceiro.Testes/DominioTeste.cs
@@ -0,0 +1,65 @@
+﻿using ProjetoFinanceiro.Domain.Dtos;
+using ProjetoFinanceiro.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoFinanceiro.Testes
+{
+    public class DominioTeste
+    {
+        public void TestarEntidade()
+        {
+            Cliente cliente = new Cliente
+            {
+                ClienteId = 1,
+                Nome = "Joao de Souza",
+                Cpf = "12345678901"
+            };
+
+            string message = $"Id: {cliente.ClienteId}, Nome: {cliente.Nome}";
+            Console.WriteLine(message);
+        }
+        public void TestarDto()
+        {
+            ClienteDto cliente = new ClienteDto
+            {
+                ClienteId = 1,
+                Nome = "Joao de Souza",
+                Cpf = "12345678901"
+            };
+
+            string message = $"Id: {cliente.ClienteId}, Nome: {cliente.Nome}";
+            Console.WriteLine(message);
+        }
+        public void TestarConversaoEntidadeParaDto()
+        {
+            Cliente cliente = new Cliente
+            {
+                ClienteId = 1,
+                Nome = "Joao de Souza",
+                Cpf = "12345678901"
+            };
+
+            ClienteDto dto = cliente.ConverterParaDto();
+
+            string message = $"Id: {dto.ClienteId}, Nome: {dto.Nome}";
+            Console.WriteLine(message);
+        }
+        public void TestarConversaoDtoParaEntidade()
+        {
+            ClienteDto cliente = new ClienteDto
+            {
+                ClienteId = 1,
+                Nome = "Joao de Souza",
+                Cpf = "12345678901"
+            };
+
+            Cliente entidade = cliente.ConverterParaEntidade();
+
+            string message = $"Id: {entidade.ClienteId}, Nome: {entidade.Nome}";
+            Console.WriteLine(message);
+        }
+
+    }
+}
